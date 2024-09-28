18120395ac06a8f3b81358c62bb802e6d3244ab4
Vladimir Elshansky <vendetta_gt@mail.ru>
2024-09-29 00:38:52 +0500

    Portal doors

---
+++ Assets/Scripts/PortalDoor.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PortalDoor : MonoBehaviour
+{
+    [SerializeField] private Transform destination;
+
+    public void Teleport(GameObject target)
+    {
+        if (destination == null)
+            return;
+
+        var controller = target.GetComponent<CharacterController>();
+        controller.enabled = false;
+        controller.transform.position = destination.position;
+        controller.transform.rotation = destination.rotation;
+        controller.enabled = true;
+    }
+}

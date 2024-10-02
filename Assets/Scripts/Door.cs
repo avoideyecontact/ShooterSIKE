6f3593ec7945a4079e92b7e9b612e6db050b59a6
Vladimir Elshansky <vendetta_gt@mail.ru>
2024-10-02 13:08:15 +0500

    door animation

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,30 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Door : MonoBehaviour, IInteractable
 {
-    [SerializeField] private bool open = false;
+    public float speed;
+    [SerializeField] private float doorRotation;
+    [SerializeField] private bool open;
     [SerializeField] private Transform edgePivotPoint;
+
+    private float localAngle = 0;
 
     private void Start()
     {
         if (open)
         {
             Open();
+            localAngle = doorRotation;
+            transform.RotateAround(edgePivotPoint.position, Vector3.up, doorRotation);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (open && localAngle < doorRotation)
+        {
+            transform.RotateAround(edgePivotPoint.position, Vector3.up, speed);
+            localAngle += speed;
+        }
+        if (!open && localAngle > 0)
+        {
+            transform.RotateAround(edgePivotPoint.position, Vector3.up, -speed);
+            localAngle -= speed;
         }
     }
 
     public void Open()
     {
         open = true;
-        transform.RotateAround(edgePivotPoint.position, Vector3.up, 90);
+        //transform.RotateAround(edgePivotPoint.position, Vector3.up, 90);
+        //transform.RotateAround(edgePivotPoint.position, Vector3.up, speed * Time.deltaTime);
     }
 
     public void Close()
     {
         open = false;
-        transform.RotateAround(edgePivotPoint.position, Vector3.down, 90);
+        //transform.RotateAround(edgePivotPoint.position, Vector3.up, -90);
+        //transform.RotateAround(edgePivotPoint.position, Vector3.up, -speed * Time.deltaTime);
     }
 
     public void Interact()
